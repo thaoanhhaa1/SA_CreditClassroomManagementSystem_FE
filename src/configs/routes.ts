f92830431cca8594c28c25e2070c34687d21fef1
Nguyễn Hiếu <hieurio12@gmail.com>
2024-05-14 17:22:59 +0700

    create login ui

--- src/configs/routes.ts
+++ src/configs/routes.ts
@@ -1,6 +1,7 @@
 const routes = {
     home: '/',
     notFound: '*',
+    login: '/login',
 };
 
 export default routes;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { routes } from '../configs';
 import { Route } from '../interfaces';
 import home from '../pages/home';
 import notFound from '../pages/notFound';
+import login from "../pages/login"
 
 const publicRoutes: Route[] = [
     { path: routes.home, element: home },
     { path: routes.notFound, element: notFound },
+    { path: routes.login, element: login}
 ];
 const privateRoutes: Route[] = [];
 
